e4f0465447c4d15a97e6909a35dbcb56d083952b
Axel Örn Sigurðsson <axel@takumi.com>
2018-02-22 21:26:49 +0100

    Add invite/remove user mutation for voyages

--- voyage/mutations/voyage.py
+++ voyage/mutations/voyage.py
@@ -5,7 +5,7 @@ from flask_login import current_user
 from voyage import fields
 from voyage.exceptions import MutationException
 from voyage.extensions import db
-from voyage.models import Media, Voyage
+from voyage.models import Media, User, Voyage
 
 
 class CreateVoyage(Mutation):
@@ -33,5 +33,66 @@ class CreateVoyage(Mutation):
         return CreateVoyage(voyage=voyage)
 
 
+class InviteUserToVoyage(Mutation):
+    class Arguments:
+        voyage_id = fields.ID(required=True, description='ID of the voyage to invite to')
+        email = fields.String(required=True, description='Email of the user to invite')
+
+    voyage = fields.Field('Voyage')
+
+    def mutate(root, info, voyage_id, email):
+        user = User.query.filter(User.email == email).first()
+
+        if user is None:
+            # TODO: Invite the user
+            raise MutationException('User not signed up')
+
+        voyage = Voyage.query.get(voyage_id)
+
+        if voyage is None:
+            raise MutationException('Voyage not found')
+
+        if user in voyage.members:
+            raise MutationException('User already a part of the mutation')
+
+        voyage.members.append(user)
+        db.session.commit()
+
+        return InviteUserToVoyage(voyage=voyage)
+
+
+class RemoveUserFromVoyage(Mutation):
+    class Arguments:
+        voyage_id = fields.ID(required=True, description='ID of the voyage to remove from')
+        email = fields.String(required=True, description='Email of the user to remove')
+
+    voyage = fields.Field('Voyage')
+
+    def mutate(root, info, voyage_id, email):
+        user = User.query.filter(User.email == email).first()
+
+        if user is None:
+            # TODO: Invite the user
+            raise MutationException('User not signed up')
+
+        voyage = Voyage.query.get(voyage_id)
+
+        if voyage is None:
+            raise MutationException('Voyage not found')
+
+        if user not in voyage.members:
+            raise MutationException("User isn't a part of the voyage")
+
+        if user is voyage.owner:
+            raise MutationException("Owners can't be removed from their voyage")
+
+        voyage.members.remove(user)
+        db.session.commit()
+
+        return RemoveUserFromVoyage(voyage=voyage)
+
+
 class VoyageMutation(object):
     create_voyage = CreateVoyage.Field()
+    invite_user_to_voyage = InviteUserToVoyage.Field()
+    remove_user_from_voyage = RemoveUserFromVoyage.Field()
