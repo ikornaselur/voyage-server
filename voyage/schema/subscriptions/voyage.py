9a83a3bb647080455e5afa8f303be4ebeb983317
Axel Örn Sigurðsson <axel@takumi.com>
2018-02-27 22:44:51 +0100

    Hook up a voyage subscription

--- voyage/schema/subscriptions/voyage.py
+++ voyage/schema/subscriptions/voyage.py
@@ -1,7 +1,4 @@
-from rx import Observable
 from rx.subjects import Subject
-
-import graphene
 
 from voyage import fields
 
@@ -9,18 +6,10 @@ voyage_subject = Subject()
 
 
 class VoyageSubscription(object):
-#     voyage = fields.Field(
-#         'Voyage',
-#         id=fields.ID(required=True),
-#     )
-# 
-#     def resolve_voyage(root, info, id):
-#         print("Whoa damn")
-#         return voyage_subject.as_observable().take_while(lambda x: True)
+    voyage = fields.Field(
+        'Voyage',
+        id=fields.ID(required=True),
+    )
 
-    count_seconds = graphene.Float(up_to=graphene.Int())
-
-    def resolve_count_seconds(root, info, up_to=5):
-        return Observable.interval(1000)\
-                         .map(lambda i: "{0}".format(i))\
-                         .take_while(lambda i: int(i) <= up_to)
+    def resolve_voyage(root, info, id):
+        return voyage_subject.filter(lambda voyage: voyage.id == id)
