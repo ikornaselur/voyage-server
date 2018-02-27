91587a0755562bef3cd90f43304fe80b811ac25f
Axel Örn Sigurðsson <axel@takumi.com>
2018-02-27 22:44:30 +0100

    Running WSGIServer debugged breaks websocket

--- server.py
+++ server.py
@@ -1,16 +1,7 @@
 from gevent.wsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
-from werkzeug.debug import DebuggedApplication
-from werkzeug.serving import run_with_reloader
 
 from voyage.application import application
 
-
-@run_with_reloader
-def run_server():
-    http_server = WSGIServer(('', 9999), DebuggedApplication(application), handler_class=WebSocketHandler)
-    http_server.serve_forever()
-
-
-if __name__ == "__main__":
-    run_server()
+http_server = WSGIServer(('', 9999), application, handler_class=WebSocketHandler)
+http_server.serve_forever()
