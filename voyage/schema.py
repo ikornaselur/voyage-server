8daac1e06e66537f290f99f00b7c88ae6a191cdc
Axel Örn Sigurðsson <axel@takumi.com>
2018-02-27 21:28:58 +0100

    Hook up subscription support and flask-gevent

--- voyage/schema.py
+++ voyage/schema.py
@@ -1,6 +1,22 @@
-from graphene import Schema
+from rx import Observable
+
+import graphene
+from graphene import ObjectType, Schema
 
 from voyage.mutations import Mutation
 from voyage.queries import Query
 
-schema = Schema(query=Query, mutation=Mutation)
+
+class Subscription(ObjectType):
+    count_seconds = graphene.Int(up_to=graphene.Int())
+
+    def resolve_count_seconds(root, info, up_to=5):
+        return (
+            Observable
+            .interval(1000)
+            .map(lambda i: "{0}".format(i))
+            .take_while(lambda i: int(i) <= up_to)
+        )
+
+
+schema = Schema(query=Query, mutation=Mutation, subscription=Subscription)
