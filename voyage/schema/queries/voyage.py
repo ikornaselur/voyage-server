51c9ebbbb87e308679d2b9e90347829ebb4877c0
Axel Örn Sigurðsson <axel@takumi.com>
2018-03-11 19:40:29 +0100

    Add comments_for_voyage query

--- voyage/schema/queries/voyage.py
+++ voyage/schema/queries/voyage.py
@@ -1,5 +1,6 @@
 import graphene
 
+from voyage.exceptions import QueryException
 from voyage.fields import ConnectionField, Field
 from voyage.models import Voyage
 
@@ -7,12 +8,22 @@ from voyage.models import Voyage
 class VoyageQuery(object):
     voyage = Field(
         'Voyage',
-        id=graphene.ID(required=True),
+        voyage_id=graphene.ID(required=True),
     )
     voyages = ConnectionField('VoyageConnection')
+    comments_for_voyage = ConnectionField(
+        'CommentConnection',
+        voyage_id=graphene.ID(required=True),
+    )
 
-    def resolve_voyage(root, info, id):
-        return Voyage.query.get(id)
+    def resolve_voyage(root, info, voyage_id):
+        return Voyage.query.get(voyage_id)
 
     def resolve_voyages(root, info):
         return Voyage.query
+
+    def resolve_comments_for_voyage(root, info, voyage_id):
+        voyage = Voyage.query.get(voyage_id)
+        if voyage is None:
+            raise QueryException('Voyage not found')
+        return voyage.comments
