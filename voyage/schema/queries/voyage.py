13154bbfa2965e5be34a27fbc404d22c0a7bd838
Axel Örn Sigurðsson <axel@takumi.com>
2018-03-11 19:44:52 +0100

    Add chapter filter to comments for voyage

--- voyage/schema/queries/voyage.py
+++ voyage/schema/queries/voyage.py
@@ -1,19 +1,20 @@
-import graphene
+from graphene.types import ID, String
 
 from voyage.exceptions import QueryException
 from voyage.fields import ConnectionField, Field
-from voyage.models import Voyage
+from voyage.models import Comment, Voyage
 
 
 class VoyageQuery(object):
     voyage = Field(
         'Voyage',
-        voyage_id=graphene.ID(required=True),
+        voyage_id=ID(required=True),
     )
     voyages = ConnectionField('VoyageConnection')
     comments_for_voyage = ConnectionField(
         'CommentConnection',
-        voyage_id=graphene.ID(required=True),
+        voyage_id=ID(required=True),
+        chapter=String(),
     )
 
     def resolve_voyage(root, info, voyage_id):
@@ -22,8 +23,15 @@ class VoyageQuery(object):
     def resolve_voyages(root, info):
         return Voyage.query
 
-    def resolve_comments_for_voyage(root, info, voyage_id):
+    def resolve_comments_for_voyage(root, info, voyage_id, chapter=None):
         voyage = Voyage.query.get(voyage_id)
         if voyage is None:
             raise QueryException('Voyage not found')
-        return voyage.comments
+        comments = Comment.query.filter(Comment.voyage == voyage)
+
+        if chapter:
+            if chapter not in voyage.chapters:
+                QueryException('Invalid chapter ({}) for the voyage'.format(chapter))
+            comments = comments.filter(Comment.chapter == chapter)
+
+        return comments
